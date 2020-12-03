3b5f8ce9ee9751d269e136d2654f5159c5f92365
maevamarin <maeva.marin@unil.ch>
2020-12-03 09:26:05 +0100

    Bookdown document

--- scripts/setup.R
+++ scripts/setup.R
@@ -10,7 +10,7 @@ packages <- c(
   "dplyr",# for modeling
   "ggrepel", "gghighlight", "patchwork", "maps", # for plotting
   "knitr", "kableExtra", "bookdown", "rmarkdown", # for the report
-  "randomForest","janitor","caret","pdftools","rvest","wordcloud2","tidytext","tokenizers","quanteda","sentimentr"
+  "randomForest","janitor","caret","pdftools","rvest","wordcloud2","tidytext","tokenizers","quanteda","sentimentr",
   "stringr","lexicon","RColorBrewer","tm","printr","ggplot2" )
 
 purrr::walk(packages, library, character.only = TRUE)
